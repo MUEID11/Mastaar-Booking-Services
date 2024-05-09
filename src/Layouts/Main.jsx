a278a3274845384296c8b53a6b0684d38e6ec7d9
MUEID11 <mehedirangpur3@gmail.com>
2024-05-10 02:42:38 +0600

    setting up developer environment

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+    return (
+        <div>
+            {/* Navbar */}
+            <h1>main</h1>
+            {/* Outlet */}
+
+            {/* Footer */}
+        </div>
+    );
+};
+
+export default Main;
